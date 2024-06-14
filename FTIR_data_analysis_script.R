042c8eeeafefbff27a121b8a9eb2aaf9f3cddc52
Harsh Sahu <harshsahu.8085@gmail.com>
2024-06-15 01:28:27 +0200

    Date Time filter in post processing

--- FTIR_data_analysis_script.R
+++ FTIR_data_analysis_script.R
@@ -1,4 +1,5 @@
 getwd()
+###### Load Library ######
 library(tidyverse)
 library(reshape2)
 library(hablar)
@@ -13,33 +14,42 @@ library(data.table)
 source("FTIR_data_cleaning_script.R")
 
 
-######### Data importing ###########
+######### Data importing & cleaning ###########
 
 #FTIR1
-#raw_path <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_1/2024-02-07_FTIR1.TXT"
-#clean_path <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
-#result_file_name <- "2024-06-03_2024-06-11_FTIR1.csv"
-#FTIR1_cleaned_data <- ftclean(raw_path, clean_path, result_file_name)
-
+raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_1/2024-06-11_FTIR1.TXT"
+clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
+result_file_name   <- "2024-06-03_2024-06-11_FTIR1.csv"
+FTIR1_cleaned_data <- ftclean(raw_path, clean_path, result_file_name)
 FTIR1_cleaned_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024-06-03_2024-06-11_FTIR1.csv")
 
-#FTIR2
-#raw_path <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_2/2024-04-09_FTIR2.TXT"
-#clean_path <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
-#result_file_name <- "2024-06-03_2024-06-11_FTIR2.csv"
-#FTIR2_cleaned_data <- ftclean(raw_path, clean_path, result_file_name)
 
+#FTIR2
+raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_2/2024-06-11_FTIR2.TXT"
+clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
+result_file_name   <- "2024-06-03_2024-06-11_FTIR2.csv"
+FTIR2_cleaned_data <- ftclean(raw_path, clean_path, result_file_name)
 FTIR2_cleaned_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024-06-03_2024-06-11_FTIR2.csv")
 
 
+
 ######### Data combining ###########
+# Convert DATE.TIME format 
+FTIR1_cleaned_data$DATE.TIME = as.POSIXct(FTIR1_cleaned_data$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
+FTIR2_cleaned_data$DATE.TIME = as.POSIXct(FTIR2_cleaned_data$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
+
+# Filter Date by measuring campaign
+start_date_time <- "2024-06-03 15:12:00"
+end_date_time <- "2024-06-10 15:14:00"
+
+FTIR1_cleaned_data <- FTIR1_cleaned_data %>% filter(DATE.TIME >= start_date_time & DATE.TIME <= end_date_time)
+FTIR2_cleaned_data <- FTIR2_cleaned_data %>% filter(DATE.TIME >= start_date_time & DATE.TIME <= end_date_time)
+
 # Set data as data.table
 data.table::setDT(FTIR1_cleaned_data)
 data.table::setDT(FTIR2_cleaned_data)
 
-FTIR1_cleaned_data$DATE.TIME = as.POSIXct(FTIR1_cleaned_data$DATE.TIME, format = "%Y-%m-%d %H:%M:%S")
-FTIR2_cleaned_data$DATE.TIME = as.POSIXct(FTIR2_cleaned_data$DATE.TIME, format = "%Y-%m-%d %H:%M:%S")
-
+# Add sufffix
 FTIR1_cleaned_data <- FTIR1_cleaned_data %>% rename_with(~paste0(., ".F1"), -DATE.TIME)
 FTIR2_cleaned_data <- FTIR2_cleaned_data %>% rename_with(~paste0(., ".F2"), -DATE.TIME)
 
