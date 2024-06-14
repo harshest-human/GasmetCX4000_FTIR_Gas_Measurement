0f097c2ea8592af334350d6873a0cd04b78ec9bf
Harsh Sahu <harshsahu.8085@gmail.com>
2024-06-15 01:29:37 +0200

    select columns and format data type

--- FTIR_data_cleaning_script.R
+++ FTIR_data_cleaning_script.R
@@ -25,6 +25,16 @@ ftclean <- function(raw_path, clean_path, result_file_name) {
         cat("Selecting final columns...\n")
         data <- data[, .(DATE.TIME, Messstelle, CO2, NH3, CH4, H2O, N2O, Acetylen)]
         
+        # Step 5: Convert columns to appropriate data types
+        cat("Converting columns to appropriate data types...\n")
+        data[, `:=`(Messstelle = as.factor(Messstelle),
+                    CO2 = as.numeric(CO2),
+                    NH3 = as.numeric(NH3),
+                    CH4 = as.numeric(CH4),
+                    H2O = as.numeric(H2O),
+                    N2O = as.numeric(N2O),
+                    Acetylen = as.numeric(Acetylen))]
+        
         # Create the output file name
         output_file <- file.path(clean_path, result_file_name)
         
@@ -35,6 +45,7 @@ ftclean <- function(raw_path, clean_path, result_file_name) {
         # Final message
         cat("Processed data has been saved as", output_file, "\n")
 }
+
 
 
 
