c3f2940a96f16d08c6b7232390a85d9f4fe482e9
Harsh Sahu <harshsahu.8085@gmail.com>
2024-11-05 13:46:36 +0100

    ftclean works perfectly

--- FTIR_data_analysis_script.R
+++ FTIR_data_analysis_script.R
@@ -16,67 +16,44 @@ source("FTIR_data_cleaning_script.R")
 
 ######### Data importing & cleaning ###########
 #FTIR1
-#raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_1/2024-06-11_FTIR1.TXT"
-#clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
-#result_file_name   <- "2024-06-03_2024-06-11_FTIR1.csv"
-#FTIR1_cleaned_data <- ftclean(raw_path, clean_path, result_file_name)
-FTIR1_cleaned_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024-06-03_2024-06-11_FTIR1.csv")
+raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_1/2024_June-Oct_FTIR1.TXT"
+clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
+result_file_name   <- "2024_June-Sep__FTIR1.csv"
+ftclean(raw_path, clean_path, result_file_name)
+FTIR.1 <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024_June-Sep__FTIR1.csv")
 
 
 #FTIR2
-#raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_2/2024-06-11_FTIR2.TXT"
-#clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
-#result_file_name   <- "2024-06-03_2024-06-11_FTIR2.csv"
-#FTIR2_cleaned_data <- ftclean(raw_path, clean_path, result_file_name)
-FTIR2_cleaned_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024-06-03_2024-06-11_FTIR2.csv")
+raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_2/2024_June-Oct_FTIR2.TXT"
+clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
+result_file_name   <- "2024_June-Sep__FTIR2.csv"
+ftclean(raw_path, clean_path, result_file_name)
+FTIR.2 <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024_June-Sep__FTIR2.csv")
 
 
 ######### Data combining ###########
 # Convert DATE.TIME format 
-FTIR1_cleaned_data$DATE.TIME = as.POSIXct(FTIR1_cleaned_data$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
-FTIR2_cleaned_data$DATE.TIME = as.POSIXct(FTIR2_cleaned_data$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
+FTIR.1$DATE.TIME = as.POSIXct(FTIR.1$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
+FTIR.2$DATE.TIME = as.POSIXct(FTIR.2$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
 
 # Filter Date by measuring campaign
 start_date_time <- "2024-06-03 15:12:00"
-end_date_time <- "2024-06-10 15:14:00"
+end_date_time <- "2024-09-18 12:03:43" 
 
-FTIR1_cleaned_data <- FTIR1_cleaned_data %>% filter(DATE.TIME >= start_date_time & DATE.TIME <= end_date_time)
-FTIR2_cleaned_data <- FTIR2_cleaned_data %>% filter(DATE.TIME >= start_date_time & DATE.TIME <= end_date_time)
+FTIR.1 <- FTIR.1 %>% filter(DATE.TIME >= start_date_time & DATE.TIME <= end_date_time)
+FTIR.2 <- FTIR.2 %>% filter(DATE.TIME >= start_date_time & DATE.TIME <= end_date_time)
 
 
 # Set data as data.table
-data.table::setDT(FTIR1_cleaned_data)
-data.table::setDT(FTIR2_cleaned_data)
+data.table::setDT(FTIR.1)
+data.table::setDT(FTIR.2)
 
 # Add sufffix
-FTIR1_cleaned_data <- FTIR1_cleaned_data %>% rename_with(~paste0(., ".F1"), -DATE.TIME)
-FTIR2_cleaned_data <- FTIR2_cleaned_data %>% rename_with(~paste0(., ".F2"), -DATE.TIME)
+FTIR.1 <- FTIR.1 %>% rename_with(~paste0(., ".F1"), -DATE.TIME)
+FTIR.2 <- FTIR.2 %>% rename_with(~paste0(., ".F2"), -DATE.TIME)
 
 # Combine two dataframes by nearest times using library(data.table)
-FTIR.comb <- FTIR1_cleaned_data[FTIR2_cleaned_data, on = .(DATE.TIME), roll = "nearest"]
+FTIR.comb <- FTIR.1[FTIR.2, on = .(DATE.TIME), roll = "nearest"]
 
-write.csv(FTIR.comb, "FTIR.comb.csv", row.names = FALSE)
+write.csv(FTIR.comb, "2024_June-Sep_FTIR.comb.csv", row.names = FALSE)
 
-FTIR.comb <- read.csv("FTIR.comb.csv")
-
-
-
-############ FTIR test ############
-raw_path           <- "D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/FTIR_2/2024-06-11_FTIR2.TXT"
-clean_path         <- "D:/Data Analysis/Gas_data/Clean_data/FTIR_clean"
-result_file_name   <- "2024-05-15_2024-05-17_FTIR.test.csv"
-FTIR2_test_data  <- ftclean(raw_path, clean_path, result_file_name)
-
-
-FTIR2_test_data <- fread("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/2024-05-15_2024-05-17_FTIR.test.csv")
-FTIR2_test_data$DATE.TIME = as.POSIXct(FTIR2_test_data$DATE.TIME, format = "%Y-%m-%dT%H:%M:%SZ")
-FTIR2_test_data <- FTIR2_test_data %>% filter(DATE.TIME >= "2024-05-15 12:39:56" & DATE.TIME <= "2024-05-17 09:39:05")
-
-# Set data as data.table
-data.table::setDT(FTIR2_test_data)
-
-# Add sufffix
-FTIR2_test_data <- FTIR2_test_data %>% rename_with(~paste0(., ".F2"), -DATE.TIME)
-
-# write
-write.csv(FTIR2_test_data, "FTIR2_test.csv", row.names = FALSE)
