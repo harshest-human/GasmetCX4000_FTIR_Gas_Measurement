6d39613ef3b21b88c356a61a1dcbac179a1515fb
Harsh Sahu <harshsahu.8085@gmail.com>
2025-06-18 13:21:41 +0200

    reshaped and hourly averaged

--- MBBM_FTIR_script.R
+++ MBBM_FTIR_script.R
@@ -32,11 +32,12 @@ MBBM_FTIR_raw$MEASUREMENT_PERIOD <- sub(".*-", "", MBBM_FTIR_raw$MEASUREMENT_PER
 MBBM_FTIR_raw <- MBBM_FTIR_raw %>% mutate(DATE.TIME = (paste(DATE_TIME, MEASUREMENT_PERIOD))) 
 MBBM_FTIR_raw <- MBBM_FTIR_raw %>% select(-MEASUREMENT_PERIOD, -DATE_TIME, -MEASUREMENT_POINT_CODE)
 MBBM_FTIR_raw$DATE.TIME <- ymd_hms(MBBM_FTIR_raw$DATE.TIME)
-MBBM_FTIR_raw$hour <- MBBM_FTIR_raw$DATE.TIME
+MBBM_FTIR_raw$hour <- floor_date(MBBM_FTIR_raw$DATE.TIME, "hour")
 MBBM_FTIR_raw <- MBBM_FTIR_raw %>% select(hour,LOCATION,CO2_DRY_PPM,CH4_DRY_PPM,NH3_DRY_PPM,H2O_VOL_PCT)
 
+
 # Calculate hourly averages
-MBBM_hourly_avg <- MBBM_FTIR_raw %>%
+MBBM_hourly <- MBBM_FTIR_raw %>%
         group_by(hour, LOCATION) %>%
         summarise(MBBM_CO2 = mean(CO2_DRY_PPM, na.rm = TRUE),
                   MBBM_CH4 = mean(CH4_DRY_PPM, na.rm = TRUE),
@@ -44,17 +45,15 @@ MBBM_hourly_avg <- MBBM_FTIR_raw %>%
                   MBBM_H2O = mean(H2O_VOL_PCT, na.rm = TRUE),
                   .groups = "drop") 
 
-reshaped_MBBM <- MBBM_hourly_avg %>%
+
+
+reshaped_MBBM_FTIR <- MBBM_hourly %>%
         pivot_wider(names_from = LOCATION,
                     values_from = c(MBBM_CO2, MBBM_CH4, MBBM_NH3, MBBM_H2O),
                     names_glue = "{.value}_{LOCATION}")
 
 
-# Convert hour to datetime format
-reshaped_MBBM$hour <- as.POSIXct(reshaped_MBBM$hour)
-
-
 # Write csv day wise
-reshaped_FTIR_14_15 <- reshaped_MBBM_FTIR %>% filter(hour >= ymd_hms("2025-04-14 00:00:00"), hour <= ymd_hms("2025-04-15 23:00:00"))
+reshaped_MBBM_FTIR <- reshaped_MBBM_FTIR %>% filter(hour >= ymd_hms("2025-04-14 00:00:00"), hour <= ymd_hms("2025-04-15 23:00:00"))
 write.csv(reshaped_FTIR_14_15,"20250414-15_hourly_FTIR1.csv" , row.names = FALSE, quote = FALSE)
 
