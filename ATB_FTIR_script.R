642fba0e873a4e484d6d4637ce0124d13a9b9e90
Harsh Sahu <harshsahu.8085@gmail.com>
2025-08-12 12:38:58 +0200

    hourly long and wide format csv

--- ATB_FTIR_script.R
+++ ATB_FTIR_script.R
@@ -34,19 +34,7 @@ ATB_FTIR.1 <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/20250408
 
 ######### Post processing ##########
 # Calculate hourly averages
-ATB_FTIR.1 <- ATB_FTIR.1 %>% 
-        mutate(CO2_ppm = CO2,
-               CH4_ppm = CH4,
-               NH3_ppm = NH3,
-               H2O_vol = H2O,
-               # Constants
-               R = 8.314472,
-               T = 273.15,
-               P = 1013.25 * 100,
-               
-               CO2_mgm3 = (CO2_ppm/1000 * 44.01 * P) / (R * T),
-               NH3_mgm3 = (NH3_ppm/1000 * 17.031 * P) / (R * T),
-               CH4_mgm3 = (CH4_ppm/1000 * 16.04 * P) / (R * T)) %>%
+ATB_FTIR.1 <- ATB_FTIR.1 %>%
         filter(Line %in% c(1, 2, 3)) %>%
         mutate(
                 location = recode(as.factor(Line),
@@ -57,30 +45,56 @@ ATB_FTIR.1 <- ATB_FTIR.1 %>%
                 analyzer = factor("FTIR.1")) %>%
         select(DATE.TIME, location, lab, analyzer, everything())
 
-# Calculate hourly averages
+###### 7.5 minute averaged intervals #######
 ATB_7.5_avg <- ATB_FTIR.1 %>%
         group_by(DATE.TIME, location, lab, analyzer) %>%
-        summarise(CO2_ppm    = mean(CO2_ppm, na.rm = TRUE),
-                  CO2_mgm3   = mean(CO2_mgm3, na.rm = TRUE),
-                  CH4_ppm    = mean(CH4_ppm, na.rm = TRUE),
-                  CH4_mgm3   = mean(CH4_mgm3, na.rm = TRUE),
-                  NH3_ppm    = mean(NH3_ppm, na.rm = TRUE),
-                  NH3_mgm3   = mean(NH3_mgm3, na.rm = TRUE),
-                  H2O_vol    = mean(H2O_vol, na.rm = TRUE),
+        summarise(CO2_ppm    = mean(CO2, na.rm = TRUE),
+                  CH4_ppm    = mean(CH4, na.rm = TRUE),
+                  NH3_ppm    = mean(NH3, na.rm = TRUE),
+                  H2O_vol    = mean(H2O, na.rm = TRUE),
                   .groups    = "drop") 
 
+#Round DATE.TIME to the nearest 450 seconds (7.5 minutes)
+round_to_interval <- function(datetime, interval_sec = 450) {
+        as.POSIXct(round(as.numeric(datetime) / interval_sec) * interval_sec, origin = "1970-01-01", tz = tz(datetime))
+}
+
+ATB_7.5_avg <- ATB_7.5_avg %>%
+        mutate(DATE.TIME = ymd_hms(DATE.TIME),
+               DATE.TIME = round_to_interval(DATE.TIME, interval_sec = 450)) %>%
+        select(DATE.TIME, location, lab, analyzer, everything())
+
 # Write csv
-ATB_7.5_avg <- ATB_7.5_avg %>% select(DATE.TIME, location, lab, analyzer, everything())
 write.csv(ATB_7.5_avg,"20250408-15_ATB_7.5_avg_FTIR.1.csv" , row.names = FALSE, quote = FALSE)
 
-# Reshape to wide format, each gas and Line combination becomes a column
-ATB_wide <- ATB_7.5_avg %>%
-        pivot_wider(names_from = c(location),
-                    values_from = c("CO2_ppm", "CO2_mgm3", "CH4_ppm", "CH4_mgm3",
-                                    "NH3_ppm", "NH3_mgm3", "H2O_vol"),
-                    names_glue = "{.value}_{location}") %>%
-        group_by(DATE.TIME, analyzer) %>%
-        summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)), .groups = "drop")
 
-# Write csv day wise
-write.csv(ATB_wide,"20250408-15_ATB_wide_FTIR.1.csv" , row.names = FALSE, quote = FALSE)
+###### hourly averaged intervals long format #######
+# Calculate hourly mean and chage pivot to long
+ATB_long <- ATB_7.5_avg %>%
+        mutate(DATE.TIME = ymd_hms(DATE.TIME)) %>%
+        mutate(DATE.TIME = floor_date(DATE.TIME, unit = "hour")) %>%
+        group_by(DATE.TIME, location, analyzer, lab) %>%
+        summarise(CO2_ppm = mean(CO2_ppm, na.rm = TRUE),
+                  CH4_ppm = mean(CH4_ppm, na.rm = TRUE),
+                  NH3_ppm = mean(NH3_ppm, na.rm = TRUE),
+                  H2O_vol = mean(H2O_vol, na.rm = TRUE),
+                  .groups = "drop")%>%
+        pivot_longer(cols = c(CO2_ppm, CH4_ppm, NH3_ppm, H2O_vol),
+                     names_to = "gas_unit",
+                     values_to = "value")
+
+# Write csv long
+write_excel_csv(ATB_long,"20250408-15_ATB_long_FTIR.1.csv")       
+
+
+###### hourly averaged intervals wide format #######
+# Reshape to wide format, each gas and Line combination becomes a column
+ATB_wide <- ATB_long %>%
+        pivot_wider(
+                names_from = c(gas_unit, location),
+                values_from = value,
+                names_sep = "_") %>%
+        arrange(DATE.TIME)
+
+# Write csv wide
+write_excel_csv(ATB_wide,"20250408-15_ATB_wide_FTIR.1.csv")    
