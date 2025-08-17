545fbc1c07f0a64513c979b158c37557ab93653d
Harsh Sahu <harshsahu.8085@gmail.com>
2025-08-17 13:58:25 +0200

    remove NA DATE.TIME

--- round to interval function.R
+++ round to interval function.R
@@ -1,5 +1,5 @@
 round_to_interval <- function(datetime, interval_sec = 450) {
-        # Convert datetime to numeric seconds since epoch
+        # Convert to numeric
         dt_num <- as.numeric(datetime)
         
         # Floor to nearest interval
@@ -8,8 +8,10 @@ round_to_interval <- function(datetime, interval_sec = 450) {
         # Convert back to POSIXct
         rounded_posix <- as.POSIXct(rounded, origin = "1970-01-01", tz = tz(datetime))
         
-        # If exact full hour (minute=0, second=0), subtract 1 second
-        is_full_hour <- format(rounded_posix, "%M:%S") == "00:00"
+        # Identify full hours (skip NAs)
+        is_full_hour <- !is.na(rounded_posix) & format(rounded_posix, "%M:%S") == "00:00"
+        
+        # Subtract 1 second for full-hour timestamps
         rounded_posix[is_full_hour] <- rounded_posix[is_full_hour] - 1
         
         return(rounded_posix)
