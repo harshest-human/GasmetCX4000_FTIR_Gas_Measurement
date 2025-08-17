15d8d21a8734d82f744a6fb64679c390e45eed5e
Harsh Sahu <harshsahu.8085@gmail.com>
2025-08-17 13:56:14 +0200

    rounding minutes to 59:59

--- round to interval function.R
+++ round to interval function.R
@@ -1,12 +1,16 @@
 round_to_interval <- function(datetime, interval_sec = 450) {
+        # Convert datetime to numeric seconds since epoch
         dt_num <- as.numeric(datetime)
         
-        # Round down to nearest interval
+        # Floor to nearest interval
         rounded <- floor(dt_num / interval_sec) * interval_sec
         
-        # If exact full hour, subtract 1 second
-        is_full_hour <- format(as.POSIXct(rounded, origin = "1970-01-01", tz = tz(datetime)), "%M:%S") == "00:00"
-        rounded[is_full_hour] <- rounded[is_full_hour] - 1
+        # Convert back to POSIXct
+        rounded_posix <- as.POSIXct(rounded, origin = "1970-01-01", tz = tz(datetime))
         
-        as.POSIXct(rounded, origin = "1970-01-01", tz = tz(datetime))
+        # If exact full hour (minute=0, second=0), subtract 1 second
+        is_full_hour <- format(rounded_posix, "%M:%S") == "00:00"
+        rounded_posix[is_full_hour] <- rounded_posix[is_full_hour] - 1
+        
+        return(rounded_posix)
 }
